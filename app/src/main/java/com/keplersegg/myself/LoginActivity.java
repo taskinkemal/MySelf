e5defd68c4806a1b9979ed874b2a3e95238bf658
Kemal Taskin <kemal.taskin@ge.com>
2018-10-26 16:56:52 +0200

    initial commit

---
+++ app/src/main/java/com/keplersegg/myself/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.keplersegg.myself;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_login);
+    }
+}
